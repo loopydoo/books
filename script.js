4065b339e1c6d5dee7004d291ab1698b92e01fcb
loopydoo <140463891+loopydoo@users.noreply.github.com>
2024-08-01 10:29:41 +1200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const input = document.getElementById("text");
-const output = document.getElementById("list");
-function add() {
-  var text = input.value;
-  output.value += `<li>${text}</li>`;
-}
